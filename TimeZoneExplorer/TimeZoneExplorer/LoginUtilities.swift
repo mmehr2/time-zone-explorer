41afc8db7377542874cfa9d8e618001c1d0e883d
Mike Mehr <mmehr2x-dev@yahoo.com>
2016-01-18 11:55:37 -0800

    Removed redundant test module.

--- TimeZoneExplorer/TimeZoneExplorer/LoginUtilities.swift
+++ TimeZoneExplorer/TimeZoneExplorer/LoginUtilities.swift
@@ -58,3 +58,49 @@ class TZClient {
     }
 
 }
+
+// Keeping this code around in case...
+//    // MARK: - Actions
+//    @IBAction func logInPressed(sender: AnyObject) {
+//        performLoginSegue()
+//        // perform some basic validation checks -- disallow blanks anyway
+//        guard let userText = userTextField.text where userText != "" else {return}
+//        guard let passwordText = passwordTextField.text where passwordText != "" else {return}
+//
+//        PFUser.logInWithUsernameInBackground(userText, password: passwordText) { user, error in
+//            if user != nil {
+//                // if successfully logged in, go to the main screen for the type of user
+//                let loginSegue = self.getLoginSegue()
+//                self.performSegueWithIdentifier(loginSegue, sender: nil)
+//            } else if let error = error {
+//                self.showErrorView(error)
+//            }
+//        }
+//    }
+//
+//    @IBAction func logOutPressed(sender: AnyObject) {
+//        PFUser.logOut()
+//        //If logout succesful:
+//        navigationController?.popToRootViewControllerAnimated(true)
+//    }
+//
+//    @IBAction func signUpPressed(sender: AnyObject) {
+//        // perform some basic validation checks -- disallow blanks anyway
+//        guard let userText = userTextField.text where userText != "" else {return}
+//        guard let passwordText = passwordTextField.text where passwordText != "" else {return}
+//
+//        // create a parse user here (rely on Parse validation?)
+//        let user = PFUser();
+//        user.username = userText
+//        user.password = passwordText
+//
+//        user.signUpInBackgroundWithBlock { succeeded, error in
+//            if (succeeded) {
+//                //The registration was successful, go to the main screen
+//                self.performSegueWithIdentifier(self.someSegue, sender: nil)
+//            } else if let error = error {
+//                //Something bad has occurred
+//                self.showErrorView(error)
+//            }
+//        }
+//    }
