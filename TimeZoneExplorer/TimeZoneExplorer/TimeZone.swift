4c57b2ee0c8bfc867a7aa4b4623af9c9305f4b4c
Mike Mehr <mmehr2x-dev@yahoo.com>
2016-01-19 13:58:46 -0800

    Refactor TZTimeZone class for better design and unit testing.

---
+++ TimeZoneExplorer/TimeZoneExplorer/TimeZone.swift
@@ -0,0 +1,102 @@
+//
+//  TimeZone.swift
+//  TimeZoneExplorer
+//
+//  Created by Michael L Mehr on 1/19/16.
+//  Copyright © 2016 Michael L. Mehr. All rights reserved.
+//
+
+/*
+Model class to abstract useful things about time zones. It includes its own NSTimeZone.
+Many things can just be done with the ZoneID, a string of the form Area/City or sometimes Area/Region/City.
+
+The func getTimeInfo() deals with the complexities of finding out the real standard offset from UTC, starting only with the current one (as of the current date). DST varies throughout the year.
+While the project spec doesn't deal with DST directly, any real world implementation will have to.
+
+Look at the ViewModel group for extensions dealing with display formatting.
+*/
+
+import Foundation
+import Parse
+
+
+class TZTimeZone {
+    
+    private let zoneID: String
+    private let zone: NSTimeZone
+    
+    init(zoneID input: String) {
+        zoneID = input
+        zone = NSTimeZone(name: zoneID)! // will crash if given invalid zoneID
+    }
+
+    /// Takes an offset in seconds, splits it into component (hours, minutes, secs=0)
+    static func splitTime( interval: NSTimeInterval ) -> (hours: Int, minutes: Int, seconds: Int) {
+        let offset = Int(Double(interval + 0.5))
+        let offabs = abs(offset)
+        let offhours = offabs/3600
+        let offmins = (offabs%3600) / 60
+        return (offhours, offmins, 0)
+    }
+    
+    enum Type {
+        case Standard
+        case Daylight
+    }
+    
+    /**
+     Returns nil if invalid zoneID.
+     
+     Otherwise, abbrev will be the abbreviation, if supported, else nil
+     
+     and Offset will be the UTC offset in seconds, if supported, else 0
+     */
+    static func getTimeInfo(zoneID: String, type: Type) -> (abbrev: String?, offset: Int)? {
+        var result: (String?, Int)? = nil
+        if let zone = NSTimeZone(name: zoneID) {
+            var offset = zone.secondsFromGMT // NOTE: this may be daylight savings time now
+            var abbrev = zone.abbreviation
+            let requestedDaylightTime = (type == .Daylight) ? true : false
+            if zone.daylightSavingTime != requestedDaylightTime {
+                if let nonDaylightDate = zone.nextDaylightSavingTimeTransition {
+                    offset = zone.secondsFromGMTForDate(nonDaylightDate)
+                    abbrev = zone.abbreviationForDate(nonDaylightDate)
+                }
+            }
+            result = (abbrev, offset)
+        }
+        return result
+    }
+    
+    /// Splits the zoneID into its components at the '/' char, returning them in order in a tuple
+    /// NOTE: Middle entry is "" if there are only two components
+    static func getDetailedName(zoneID: String) -> (area: String, region: String, city: String) {
+        let segs = zoneID.componentsSeparatedByString("/")
+        switch segs.count {
+        case 1: return (segs[0], "", "")
+        case 2: return (segs[0], "", segs[1])
+        case 3: return (segs[0], segs[1], segs[2])
+        default: return ("", "", "")
+        }
+    }
+
+    /// Single-output version of getDetailedName() (used internally)
+    static func getAreaName(zoneID: String) -> String {
+        let (output, _, _) = getDetailedName(zoneID)
+        return output
+    }
+    
+    /// Single-output version of getDetailedName() (used internally)
+    static func getRegionName(zoneID: String) -> String {
+        let (_, output, _) = getDetailedName(zoneID)
+        return output
+    }
+    
+    /// Single-output version of getDetailedName() (used internally)
+    static func getCityName(zoneID: String) -> String {
+        let (_, _, output) = getDetailedName(zoneID)
+        return output
+    }
+    
+}
+
--- TimeZoneExplorer/TimeZoneExplorer/TimeZoneUtilities.swift
+++ TimeZoneExplorer/TimeZoneExplorer/TimeZoneUtilities.swift
@@ -6,11 +6,17 @@
 //  Copyright © 2016 Michael L. Mehr. All rights reserved.
 //
 
+/*
+These functions relate to the ViewModel, or formatting and support for Views.
+See Model-View-ViewModel pattern wiki: https://en.wikipedia.org/wiki/Model–view–viewmodel
+It's mostly about formatting the various fields needed for list and detailed display of the Model class in the various ViewController classes.
+*/
+
 import Foundation
 //import Parse
 //import ParseUI
 
-class TZTimeZone {
+extension TZTimeZone {
 
     /// Returns the offset formatted as sHH:MM, where
     ///   s is the sign (+ or -)
@@ -18,56 +24,27 @@ class TZTimeZone {
     ///   MM is minutes (always with two digits)
     /// Any seconds are truncated (see Dublin Mean Time in 1900, for example)
     static func getStandardFormatOffset( offsetSecs: Int ) -> String {
-        let offset = offsetSecs
-        let offabs = abs(offset)
-        let offhours = offabs/3600
-        let offmins = (offabs%3600) / 60
+        let interval = NSTimeInterval(offsetSecs)
+        let sign = (offsetSecs < 0) ? "-" : "+"
+        let (offhours, offmins, _) = splitTime(interval)
         let offlead = (offhours < 10) ? "0" : "" // kludge to add leading 0 for hours<10
         let offextra = (offmins == 0) ? "0" : "" // kludge to prevent a single 0 when 0 mins
-        let offstr = "\(offset<0 ? "-" : "+")\(offlead)\(offhours):\(offmins)\(offextra)"
+        let offstr = "\(sign)\(offlead)\(offhours):\(offmins)\(offextra)"
         return offstr
     }
-    
-    enum Type {
-        case Standard
-        case Daylight
-    }
 
-    /**
-     Returns nil if invalid zoneID.
-     
-     Otherwise, abbrev will be the abbreviation, if supported, else nil
-     
-     and Offset will be the UTC offset in seconds, if supported, else 0
-     */
-    static func getTimeInfo(zoneID: String, type: Type) -> (abbrev: String?, offset: Int)? {
-        var result: (String?, Int)? = nil
-        if let zone = NSTimeZone(name: zoneID) {
-            var offset = zone.secondsFromGMT // NOTE: this may be daylight savings time now
-            var abbrev = zone.abbreviation
-            let requestedDaylightTime = (type == .Daylight) ? true : false
-            if zone.daylightSavingTime != requestedDaylightTime {
-                if let nonDaylightDate = zone.nextDaylightSavingTimeTransition {
-                    offset = zone.secondsFromGMTForDate(nonDaylightDate)
-                    abbrev = zone.abbreviationForDate(nonDaylightDate)
-                }
-            }
-            result = (abbrev, offset)
-        }
-        return result
-    }
-
+    // NSDateFormatters are expensive, use only one
     private static var formatter = NSDateFormatter()
     
-    /// Given:
-    /// @param date - NSDate to format
-    /// @param zoneID - String representing name of time zone to use
+    /// Given params:
+    ///     date - NSDate to format
+    ///     zoneID - String representing name of time zone to use
     /// Returns:
-    /// tuple of four values (date, time, ampm, abb) as all Strings
-    /// @returns date - medium date format MMM DD, YYYY
-    /// @returns time - medium time style HH:MM:SS
-    /// @returns ampm - the current AM/PM symbol (12-hour clock)
-    /// @returns abb - the current abbreviation for the time type (std or daylight)
+    /// A tuple of four values (date, time, ampm, abb) as all Strings
+    ///     date - medium date format MMM DD, YYYY
+    ///     time - medium time style HH:MM:SS
+    ///     ampm - the current AM/PM symbol (12-hour clock)
+    ///     abb - the current abbreviation for the time type (std or daylight)
     static func getClock( date: NSDate, formattedForZone zoneID: String ) -> (date: String, time: String, ampm: String, abb: String) {
         var result = ("No date", "..:..:..", "..", "...")
         if let zone = NSTimeZone(name: zoneID) {
@@ -117,16 +94,6 @@ class TZTimeZone {
             result = stdFormatInput.substringToIndex(foundRange.startIndex)
         }
         return result
-    }
-    
-    static func getDetailedName(zoneID: String) -> (area: String, region: String, city: String) {
-        let segs = zoneID.componentsSeparatedByString("/")
-        switch segs.count {
-        case 1: return (segs[0], "", "")
-        case 2: return (segs[0], "", segs[1])
-        case 3: return (segs[0], segs[1], segs[2])
-        default: return ("", "", "")
-        }
     }
     
 }
