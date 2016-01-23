ed32353cfa7ef4f8145a23eb7618fe8159152f3c
mike@azuresults.com <mmehr2x-dev@yahoo.com>
2016-01-23 04:18:53 -0800

    Refactor TZClient class.

--- TimeZoneExplorer/TimeZoneExplorer/Client.swift
+++ TimeZoneExplorer/TimeZoneExplorer/Client.swift
@@ -10,5 +10,57 @@ import Foundation
 import Parse
 
 class TZClient {
+
+    
+    enum LoginState {
+        case NoUser
+        case NotAuthenticated
+        case Authenticated
+    }
+    
+    static func getLoginState() -> LoginState {
+        var result = LoginState.NoUser
+        if let user = PFUser.currentUser() {
+            if user.authenticated {
+                result = .Authenticated;
+            } else {
+                result = .NotAuthenticated
+            }
+        }
+        return result
+    }
+    
+    static var loggedIn: Bool {
+        return TZClient.getLoginState() == .Authenticated
+    }
+    
+    static func logoutCurrentUser() {
+        PFUser.logOut()
+    }
+    
+    static func loginUser(username: String, password: String, completion: ((TZClient?, String?) -> ())? = nil) {
+        PFUser.logInWithUsernameInBackground(username, password: password) { user, error in
+        }
+    }
+    
+    // MARK: role functions
+    enum Role : CustomStringConvertible {
+        case User
+        case Manager
+        case Administrator
+        
+        var description: String {
+            switch self {
+            case .User: return "User"
+            case .Manager: return "Manager"
+            case .Administrator: return "Administrator"
+            }
+        }
+    }
+    
+    static var role: Role {
+        // TBD: figure out from current PFUser role
+        return Role.User
+    }
     
 }
--- TimeZoneExplorer/TimeZoneExplorer/LoginUtilities.swift
+++ TimeZoneExplorer/TimeZoneExplorer/LoginUtilities.swift
@@ -12,56 +12,6 @@ import ParseUI
 
 // needs of project for its User class (encompasses all roles: User, Manager, Administrator)
 extension TZClient {
-
-    enum LoginState {
-        case NoUser
-        case NotAuthenticated
-        case Authenticated
-    }
-    
-    static func getLoginState() -> LoginState {
-        var result = LoginState.NoUser
-        if let user = PFUser.currentUser() {
-            if user.authenticated {
-                result = .Authenticated;
-            } else {
-                result = .NotAuthenticated
-            }
-        }
-        return result
-    }
-    
-    static var loggedIn: Bool {
-        return TZClient.getLoginState() == .Authenticated
-    }
-    
-    static func logoutCurrentUser() {
-        PFUser.logOut()
-    }
-    
-    static func loginUser(username: String, password: String, completion: ((TZClient?, String?) -> ())? = nil) {
-        PFUser.logInWithUsernameInBackground(username, password: password) { user, error in
-        }
-    }
-    
-    enum Role : CustomStringConvertible {
-        case User
-        case Manager
-        case Administrator
-    
-        var description: String {
-            switch self {
-            case .User: return "User"
-            case .Manager: return "Manager"
-            case .Administrator: return "Administrator"
-            }
-        }
-    }
-    
-    static var role: Role {
-        // TBD: figure out from current PFUser role
-        return Role.User
-    }
     
     static func getLoginViewControllerFor(presentingVC: UIViewController) -> PFLogInViewController {
         let logInController = TZLogInViewController()
