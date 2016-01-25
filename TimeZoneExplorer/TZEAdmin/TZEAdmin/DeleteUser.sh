246efa1981fabd6a4e7c7b09f453d2209e3ede64
mike@azuresults.com <mmehr2x-dev@yahoo.com>
2016-01-25 01:24:42 -0800

    Added shell script to manage UserDelete action (hard to do from app). Mod comment in Logout.sh as well (both invalidate the session token in the environment).

--- TimeZoneExplorer/TZEAdmin/TZEAdmin/DeleteUser.sh
+++ TimeZoneExplorer/TZEAdmin/TZEAdmin/DeleteUser.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-#  Logout.sh
+#  DeleteUser.sh
 #  TZEAdmin
 #
 #  Created by Michael L Mehr on 1/24/16.
 #  Copyright © 2016 Michael L. Mehr. All rights reserved.
 
-# Log out the currently logged in user.
+# Delete authenticated user within a valid login session.
 
 # App ID is placed in $PARSE_APP_ID
 # Access key is placed in $PARSE_MASTER_KEY
 # Valid session token (returned by login or signup) must be in $PARSE_SESSION_TOKEN.
-# The token is removed once logout is completed (no check for failure).
+# Parameter is Object ID for the _User object to be deleted.
 
-# echo curl -X POST
-curl -X POST \
+# Turns out that no _Role:users management is needed; the links are autodeleted.
+
+# Output is pretty-printed with Python -m json.tool.
+
+# echo curl -X DELETE
+curl -X DELETE \
 -H "X-Parse-Application-Id: $PARSE_APP_ID" \
 -H "X-Parse-REST-API-Key: $PARSE_RESTAPI_KEY" \
 -H "X-Parse-Session-Token: $PARSE_SESSION_TOKEN" \
-https://api.parse.com/1/logout
+https://api.parse.com/1/users/$1 | python -m json.tool
 
 # This also logs out the session token, which is no longer valid after the operation above.
 unset PARSE_SESSION_TOKEN
